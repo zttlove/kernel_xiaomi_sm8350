e466cf5dced2477702fe34bd2c760636e813ce8d
zttlove <115808960+zttlove@users.noreply.github.com>
2026-04-26 12:09:55 +0800

    Update inode.c

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -602,7 +602,7 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
         extern bool ksu_devpts_hook;
         extern int ksu_handle_devpts(struct inode*);
     #endif
-#endif
+
 
 /**
  * devpts_get_priv -- get private data for a slave
@@ -618,7 +618,7 @@ void *devpts_get_priv(struct dentry *dentry)
 		ksu_handle_devpts(dentry->d_inode);
 	}
 #endif
-#endif
+
 
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
